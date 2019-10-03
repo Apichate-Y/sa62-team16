c516e9520fb0255964f2679c70b366e252121740
GlomGlom <apichate.yatra@gmail.com>
2019-10-03 12:31:07 +0700

    เพิ่มข้อมูล Admin

---
+++ back-end/src/main/java/com/sa/team16/Dataloader/AdminDataloader.java
@@ -0,0 +1,28 @@
+package com.sa.team16.Dataloader;
+
+import com.sa.team16.Entity.*;
+import com.sa.team16.Repository.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminDataloader implements ApplicationRunner {
+
+	@Autowired
+	private AdminRepository adminRepository;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+		Admin Admin = new Admin();
+		Admin.setAdminpassword("Admin");
+		Admin.setAdminpassword("Admin");
+		adminRepository.save(Admin);
+
+		adminRepository.findAll().forEach(System.out::println);
+
+	};
+}
