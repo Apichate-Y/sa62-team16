479c9cedd098f1e66e81a0edfbca9c4952e7ef9e
GlomGlom <apichate.yatra@gmail.com>
2019-10-03 17:49:04 +0700

    demo "หน้าสมัครกิจกรรมชมรม"

--- front-end/src/views/dashboard.vue
+++ front-end/src/views/dashboard.vue
@@ -1,15 +1,26 @@
 <template>
-  <div id="app">
-    <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+  <div>
+    <NavBar></NavBar>
+    <v-flex class="mt-2">
+      <v-card max-width="300" height="180" class="mx-auto">
+        <v-row justify=center>
+          <v-flex class="mt-12">
+            <div class="text-center">
+              <v-btn class="primary" min-width="150" min-height="80" @click="$router.push('/registeractivity')">สมัครกิจกรรมชมรม</v-btn>
+            </div>
+          </v-flex>
+        </v-row>
+      </v-card>
+    </v-flex>
   </div>
 </template>
 
 <script>
+import NavBar from "../components/NavBar";
 export default {
-
-}
+  name: "Dashboard",
+  components: {
+    NavBar
+  }
+};
 </script>
-
-<style>
-
-</style>
