ad5a4c909aba2815eb13ad9978ccc4a2160a3a13
GlomGlom <apichate.yatra@gmail.com>
2019-10-13 22:11:24 +0700

    ปรับ UI

--- front-end/src/components/MainLogin.vue
+++ front-end/src/components/MainLogin.vue
@@ -1,40 +1,42 @@
 <template>
-  
-  <v-card>
-    <v-carousel cycle height="690" hide-delimiter-background show-arrows-on-hover>
+  <div>
+    <v-toolbar flat height="120px" tile color="deep-purple">
+      <v-toolbar-title class="text-uppercase white--text display-1 font-weight-medium">
+        <span>sut</span>
+        <span class="font-weight-light">club</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text class="white--text" @click="$router.push('/adminlogin')">
+        <v-icon left>person</v-icon>Admin
+      </v-btn>
 
-      <v-carousel-item v-for="(item,i) in itemss" :key="i" :src="item.src">
-                <div class="display-1"> </div>      
-      </v-carousel-item>
+      <v-btn text class="white--text" @click="$router.push('/userlogin')">
+        <v-icon left>people_alt</v-icon>Member
+      </v-btn>
+    </v-toolbar>
 
-
+    <v-carousel height="710" tile cycle hide-delimiter-background show-arrows-on-hover>
+      <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
     </v-carousel>
-
-    <v-card-actions class="justify-center">
-      <v-btn text color="primary" @click="$router.push('/adminlogin')">Admin</v-btn>
-      <v-btn rounded color="primary" dark large @click="$router.push('/userlogin')">Member</v-btn>
-    </v-card-actions> 
-  </v-card>
+  </div>
 </template>
 
 <script>
-
-  export default {
-    data () {
-      return {
-        itemss: [
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          },
-           {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-          },
-         {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-          },
-        ],
-      }
-    },
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: require("../assets/Slide-1.jpg")
+        },
+        {
+          src: require("../assets/Slide-2.jpg")
+        },
+        {
+          src: require("../assets/Slide-3.jpg")
+        }
+      ]
+    };
   }
-
+};
 </script>
